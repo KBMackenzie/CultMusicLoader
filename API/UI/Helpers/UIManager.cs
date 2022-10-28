8b52d75c35b61901d5fd0774aad3e174a002924c
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2022-10-28 09:33:26 -0300

    UIManager clean-up

--- API/UI/Helpers/UIManager.cs
+++ API/UI/Helpers/UIManager.cs
@@ -5,11 +5,9 @@ using CultMusicLoader.UI.Patches;
 namespace CultMusicLoader.UI.Helpers;
 public static class UIManager
 {
-    // UI Layer
     static LayerMask _UILayer = LayerMask.NameToLayer("UI");
     public static LayerMask UILayer => _UILayer;
 
-    // UI Helper methods:
     public static void AddToPauseMenu<T>() where T : UIMenuBase
     {
         UIPatches.PauseMenuQueue.Add(typeof(T));
@@ -32,9 +30,9 @@ public static class UIManager
     {
         GameObject obj = new GameObject(name);
         obj.layer = UILayer;
+        obj.transform.SetParent(parent);
         obj.transform.localPosition = Vector3.zero;
         obj.transform.localScale = Vector3.one;
-        obj.transform.SetParent(parent);
         return obj;
     }
 }
